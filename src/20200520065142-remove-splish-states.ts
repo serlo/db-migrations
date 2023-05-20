8f1fac1bfca6899881e50e2dea12ea0a3850de46
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-05-21 00:57:09 +0200

    fix: import legacy-editor-to-editor from legacy as module

--- src/20200520065142-remove-splish-states.ts
+++ src/20200520065142-remove-splish-states.ts
@@ -19,7 +19,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/serlo.org for the canonical source repository
  */
-import { convertSplishToEdtrIO } from '@serlo/legacy-editor-to-editor'
+import { convertSplishToEdtrIO } from './legacy-editor-to-editor'
 
 import { createMigration } from './utils'
 
