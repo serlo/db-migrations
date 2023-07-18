5bd131cdf4e3ed1cc25a0d6b5f885304fd5f55d3
eliflores <eli@serlo.org>
2023-07-18 18:03:21 +0200

    WIP Fix `yarn test`
    Co-authored-by: MoeHome <moritz+home@serlo.org>

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types'
+
+const jestConfig: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/'],
+}
+
+export default jestConfig
