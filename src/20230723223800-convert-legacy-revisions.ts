7c5cf13a541c62a14c119e2c2c5c79ba3b270e10
MoeHome <moritz+home@serlo.org>
2023-07-28 15:16:56 +0200

    WIP: migration code + added missing dependencies
    Co-authored-by: Eli Flores <eli@serlo.org>

--- src/20230723223800-convert-legacy-revisions.ts
+++ src/20230723223800-convert-legacy-revisions.ts
@@ -1,4 +1,5 @@
-import { convert, Edtr, Legacy } from '@serlo/legacy-editor-to-editor'
+import { convert, Edtr, Legacy } from '../archive/legacy-editor-to-editor'
+import { createMigration } from './utils'
 
 // Follow ups:
 // run table to serloTable conversion again
@@ -6,6 +7,36 @@ import { convert, Edtr, Legacy } from '@serlo/legacy-editor-to-editor'
 // auto-convert layout plugin to either multimedia or rows plugin
 // maybe run add-image-caption mutation again
 // maybe run migrate-equations, add-first-explanation-to-equation and add-transformation-target-to-equations again
+
+
+
+
+createMigration(module.exports, {
+  up: async (db) => {
+    const result = await db.runSql<{id: number, description: string}[]>(`
+      SELECT id, description
+      FROM term_taxonomy
+      WHERE description LIKE '[%'
+      LIMIT 1
+      `
+    )
+    //console.log(result)
+    //const convertedDescriptions = result.map(row => convertOrReturnInput(row.description))
+
+
+    const response = await db.runSql<{id: number, entity_revision_id:number, value: string}[]>(`
+      SELECT id, entity_revision_id, value
+      FROM entity_revision_field
+      WHERE field = 'content' AND value LIKE '[%'
+      LIMIT 1
+    `)
+
+
+    console.log(response)
+    response.map(row => convertOrReturnInput(row.value))
+  }
+
+})
 
 function convertOrReturnInput(input?: string) {
   if (!input) {
@@ -22,8 +53,11 @@ function convertOrReturnInput(input?: string) {
 
     const legacy = input as Legacy
     // fixes https://github.com/serlo/frontend/issues/1563
+
     const sanitized = JSON.parse(JSON.stringify(legacy).replace(/```/g, ''))
+    console.log(`sanitized content: ${sanitized}`)
     const converted = convert(sanitized) as Edtr
+    console.log(`converted content ${converted}`)
 
     const serialized =
       !converted || typeof converted === 'string'
