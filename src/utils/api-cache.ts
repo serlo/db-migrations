547cb1400bae74762d1ee48f3332db0e87a885da
Stephan Kulla <git.mail@kulla.me>
2023-08-16 20:51:13 +0200

    feat(api-cache): Add src/utils/api-cache.ts

---
+++ src/utils/api-cache.ts
@@ -0,0 +1,22 @@
+import Redis from 'ioredis'
+
+export class ApiCache {
+  private redis: Redis
+
+  constructor() {
+    if (typeof process.env.REDIS_URL === 'string') {
+      this.redis = new Redis(process.env.REDIS_URL)
+    } else {
+      throw new Error('Env `REDIS_URL` is not defined')
+    }
+  }
+
+  public async quit() {
+    await this.redis.quit()
+  }
+
+  public async deleteUuid(uuid: number) {
+    await this.redis.del(`de.serlo.org/api/uuid/${uuid}`)
+    console.log(`INFO: API cache for UUID ${uuid} deleted`)
+  }
+}
