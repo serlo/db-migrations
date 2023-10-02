903f4ea935b7315fb0c68d6c3c612e39b8ab23b8
Stephan Kulla <git.mail@kulla.me>
2023-10-02 23:56:26 +0200

    Fix src/20230917105600-upgrade-all-edtr-io-states.ts
    Copy code from archived source file

--- src/20230917105600-upgrade-all-edtr-io-states.ts
+++ src/20230917105600-upgrade-all-edtr-io-states.ts
@@ -1,5 +1,10 @@
 import { function as f } from 'fp-ts'
-import { createEdtrIoMigration } from './utils'
+import * as t from 'io-ts'
+import {
+  createEdtrIoMigration,
+  replacePluginState,
+  transformPlugins,
+} from './utils'
 
 import { addImageCaption } from './20220517163100-add-image-caption'
 import { addFirstExplanationToEquation } from './20210923155000-add-first-explanation-to-equation'
@@ -7,9 +12,9 @@ import { addTransformationTargetToEquation } from './20210923231900-add-transfor
 import { convertImportantAndBlockquoteToBox } from './20220625202000-convert-important-and-blockquote-to-box'
 import { convertTableToSerloTable } from './20230526090001-convert-table-to-serlo-table'
 import {
-  migrateEquations,
-  removeLayoutPlugins,
-} from './20230723223800-convert-legacy-revisions'
+  migrateEquationsState,
+  LegacyEquationsPluginState,
+} from './20201101234700-migrate-equations'
 
 createEdtrIoMigration({
   exports: module.exports,
@@ -29,3 +34,65 @@ function convertEdtrIo(content: unknown) {
     removeLayoutPlugins,
   )
 }
+
+const LegacyEquationsState = t.type({
+  steps: t.array(
+    t.type({
+      left: t.type({ plugin: t.literal('text') }),
+      right: t.type({ plugin: t.literal('text') }),
+      transform: t.type({ plugin: t.literal('text') }),
+      sign: t.string,
+    }),
+  ),
+})
+
+const migrateEquations = replacePluginState({
+  equation: ({ state }) => {
+    if (LegacyEquationsState.is(state)) {
+      return migrateEquationsState(
+        state as unknown as LegacyEquationsPluginState,
+      )
+    } else {
+      return document
+    }
+  },
+})
+
+const PluginDecoder = t.type({ plugin: t.string, state: t.unknown })
+
+const RowsPluginDecoder = t.type({
+  plugin: t.literal('rows'),
+  state: t.array(PluginDecoder),
+})
+
+const LayoutPluginDecoder = t.type({
+  plugin: t.literal('layout'),
+  state: t.array(
+    t.type({
+      width: t.number,
+      child: t.unknown,
+    }),
+  ),
+})
+
+const removeLayoutPlugins = transformPlugins({
+  layout: (value) => {
+    if (LayoutPluginDecoder.is(value)) {
+      return value.state.flatMap(({ child }) => {
+        if (RowsPluginDecoder.is(child)) {
+          return child.state
+        } else if (PluginDecoder.is(child)) {
+          return [child]
+        } else {
+          throw new Error(
+            `Illegal value (remove layout plugin): ${JSON.stringify(value)}`,
+          )
+        }
+      })
+    } else {
+      throw new Error(
+        `Illegal value (remove layout plugin): ${JSON.stringify(value)}`,
+      )
+    }
+  },
+})
