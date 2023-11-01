f34efc9e1f21fcb633aff5b6e7e763a3dc8f5441
Stephan Kulla <git.mail@kulla.me>
2023-11-01 19:10:15 +0100

    WIP: Add algorithm to transform revisions

--- src/20231030171900-merge-solutions-into-exercises.ts
+++ src/20231030171900-merge-solutions-into-exercises.ts
@@ -2,6 +2,7 @@ import * as t from 'io-ts'
 
 import { Database, createMigration } from './utils'
 import { ApiCache } from './utils/api-cache'
+import assert from 'assert'
 
 const SolutionContentDecoder = t.type({
   plugin: t.literal('solution'),
@@ -29,15 +30,13 @@ createMigration(exports, {
   up: async (db) => {
     const apiCache = new ApiCache()
     const batchSize = 5000
-    let entities: Entity[] = []
+    let entities: Row[] = []
 
     do {
       const lastId = entities.at(-1)?.entityId ?? 0
-      entities = await db.runSql<Entity[]>(
+      entities = await db.runSql<Row[]>(
         `select
-           entity.id as entityId,
-           entity.license_id as licenseId,
-           entity.current_revision_id as currentRevisionId
+           entity.id as entityId
          from entity
          join type on entity.type_id = type.id
          where type.name = "text-exercise"
@@ -48,126 +47,267 @@ createMigration(exports, {
       )
 
       for (const entity of entities) {
-        await updateExercise(db, apiCache, entity)
+        await updateExercise(db, apiCache, entity.entityId)
+
+        // TODO
+        break
       }
+
+      // TODO
+      break
     } while (entities.length > 0)
 
     await apiCache.quit()
+
+    interface Row {
+      entityId: number
+    }
   },
 })
 
 async function updateExercise(
   db: Database,
   apiCache: ApiCache,
-  exercise: Entity,
+  exerciseId: number,
 ) {
-  // TODO: Fix this case
-  if (exercise.currentRevisionId == null) return
+  const exercise = await loadEntityTree(db, exerciseId)
+  const newRevisions = transformEntity(exercise)
 
-  const solution = await getSolution(db, exercise)
+  for (const revision of newRevisions) {
+    console.log('---')
+    printTree((x) => (x == null ? 'null' : x.id), revision.current)
+  }
+}
 
-  if (solution === null) return
+function transformEntity(
+  entity: TreeNode<EntityWithRevisions>,
+): TransformResult[] {
+  let previous: TreeNode<EntityWithRevision | null> = mapTree(
+    () => null,
+    entity,
+  )
+  let rest = entity
+  let result: TransformResult[] = []
 
-  let exerciseContent = await getContent(db, exercise)
+  while (getValues(rest).some((entity) => entity.revisions.length > 0)) {
+    const dates = getValues(rest)
+      .map((entity) => entity.revisions.at(0)?.date ?? null)
+      .filter(isNotNull)
+    const minDate = dates.reduce((a, b) => (a <= b ? a : b))
 
-  if (RowPluginDecoder.is(exerciseContent)) {
-    exerciseContent = {
-      plugin: 'exercise',
-      state: { content: exerciseContent },
+    const splitResult = split(rest, minDate)
+
+    result.push({ previous, current: splitResult.current })
+
+    previous = splitResult.current
+    rest = splitResult.rest
+  }
+
+  return result
+}
+
+interface TransformResult {
+  previous: TreeNode<EntityWithRevision | null>
+  current: TreeNode<EntityWithRevision | null>
+}
+
+function split(
+  node: TreeNode<EntityWithRevisions>,
+  date: Date,
+): {
+  current: TreeNode<EntityWithRevision | null>
+  rest: TreeNode<EntityWithRevisions>
+} {
+  let currentValue: EntityWithRevision | null
+  let restValue: EntityWithRevisions
+
+  if (
+    node.value.revisions.length > 0 &&
+    node.value.revisions[0].date.getTime() < date.getTime() + 10
+  ) {
+    const base = {
+      id: node.value.id,
+      licenseId: node.value.licenseId,
+      typeName: node.value.typeName,
     }
-  }
-
-  if (!ExerciseContentDecoder.is(exerciseContent)) {
-    throw new Error(
-      `Illegal content for exercise ${exercise.entityId} with current revision ${exercise.currentRevisionId}`,
-    )
-  }
-
-  if (exerciseContent.state.solution != null) return
-
-  let solutionContent = await getContent(db, solution)
-
-  if (solutionContent == null) return
-
-  if (RowPluginDecoder.is(solutionContent)) {
-    solutionContent = {
-      plugin: 'solution',
-      state: {
-        strategy: { plugin: 'text' },
-        steps: solutionContent,
-      },
-    }
-  }
-
-  if (!SolutionContentDecoder.is(solutionContent)) {
-    throw new Error(
-      `Illegal content for solution ${solution.entityId} with current revision ${solution.currentRevisionId}`,
-    )
-  }
-
-  if (exercise.licenseId !== solution.licenseId) {
-    solutionContent.state['licenseId'] = solution.licenseId
-  }
-
-  exerciseContent.state['solution'] = solutionContent
-
-  await db.runSql(
-    ` update entity_revision_field set value = ?
-      where entity_revision_id = ? and field = "content"`,
-    JSON.stringify(exerciseContent),
-    exercise.currentRevisionId,
-  )
-
-  await apiCache.deleteUuid(exercise.currentRevisionId)
-}
-
-async function getSolution(db: Database, entity: Entity) {
-  // TODO: There are exercises with more than 1 solution -> fix it...
-  const result = await db.runSql<Entity[]>(
-    ` select
-        solution.id as entityId,
-        solution.license_id as licenseId,
-        solution.current_revision_id as currentRevisionId
-      from entity_link
-      join entity solution on solution.id = entity_link.child_id
-      join type solution_type on solution.type_id = solution_type.id
-      where
-        solution_type.name = "text-solution"
-        and entity_link.parent_id = ?
-      limit 1`,
-    entity.entityId,
-  )
-
-  if (result.length === 1) {
-    return result[0]
-  } else if (result.length === 0) {
-    return null
+    currentValue = { ...base, revision: node.value.revisions[0] }
+    restValue = { ...base, revisions: node.value.revisions.slice(1) }
   } else {
-    throw new Error(`Exercise ${entity.entityId} has more than one solution`)
+    currentValue = null
+    restValue = node.value
+  }
+
+  const children = node.children.map((child) => split(child, date))
+  const currentChildren = children.map((x) => x.current)
+  const restChildren = children.map((x) => x.rest)
+
+  return {
+    current: { value: currentValue, children: currentChildren },
+    rest: { value: restValue, children: restChildren },
   }
 }
 
-async function getContent(db: Database, entity: Entity) {
-  if (entity.currentRevisionId == null) return null
+async function loadEntityTree(
+  db: Database,
+  entityId: number,
+): Promise<TreeNode<EntityWithRevisions>> {
+  const entity = await loadEntity(db, entityId)
+  let childIds: number[] = []
 
-  const result = await db.runSql<{ content: string }[]>(
-    ` select value as content
-      from entity_revision_field
-      where entity_revision_id = ? and field = "content"`,
-    entity.currentRevisionId,
+  if (entity.typeName === TypeName.ExerciseType) {
+    childIds = await loadChildrenIds({
+      db,
+      entityId: entityId,
+      childType: TypeName.SolutionType,
+      limit: 1,
+    })
+  }
+
+  const children = await Promise.all(
+    childIds.map((childId) => loadEntityTree(db, childId)),
   )
 
-  if (result.length === 1) {
-    return JSON.parse(result[0].content) as unknown
-  } else {
-    throw new Error(
-      `Revision ${entity.currentRevisionId} has more than one or no content`,
-    )
-  }
+  return { value: entity, children }
 }
 
-interface Entity {
+async function loadChildrenIds({
+  db,
+  entityId,
+  childType,
+  limit,
+}: {
+  db: Database
   entityId: number
+  childType: TypeName
+  limit?: number
+}): Promise<number[]> {
+  let sqlCommand = `
+      select child_id as childId
+      from entity_link
+      join entity child on child.id = entity_link.child_id
+      join type child_type on child_type.id = child.type_id
+      where parent_id = ? and child_type.name = ?`
+
+  if (limit != null) sqlCommand += ` limit ${limit}`
+
+  const result = await db.runSql<{ childId: number }[]>(
+    sqlCommand,
+    entityId,
+    childType,
+  )
+
+  return result.map((row) => row.childId)
+}
+
+async function loadEntity(
+  db: Database,
+  entityId: number,
+): Promise<EntityWithRevisions> {
+  const entityBaseResult = await db.runSql<EntityBase[]>(
+    ` select
+        entity.id as id,
+        entity.license_id as licenseId,
+        type.name as typeName
+      from entity
+      join type on entity.type_id = type.id
+      where entity.id = ?`,
+    entityId,
+  )
+  const entityBase = pickFirst(entityBaseResult)
+
+  assert(
+    isTypeName(entityBase.typeName),
+    `Entity ${entityId} has unsupported type name`,
+  )
+  const revisions = await loadRevisions(db, entityId)
+
+  return { ...entityBase, revisions }
+}
+
+async function loadRevisions(
+  db: Database,
+  entityId: number,
+): Promise<Revision[]> {
+  return await db.runSql<Revision[]>(
+    ` select
+          entity_revision.id,
+          entity_revision.date,
+          entity_revision_field.value as content
+      from entity_revision
+      left join entity_revision_field on
+          entity_revision_field.entity_revision_id = entity_revision.id
+          and entity_revision_field.field = "content"
+      where entity_revision.repository_id = ?
+      order by entity_revision.date`,
+    entityId,
+  )
+}
+
+function mapTree<A, B>(mapper: (x: A) => B, node: TreeNode<A>): TreeNode<B> {
+  return {
+    value: mapper(node.value),
+    children: node.children.map((child) => mapTree(mapper, child)),
+  }
+}
+
+function getValues<V>(node: TreeNode<V>): V[] {
+  return [node.value, ...node.children.flatMap(getValues)]
+}
+
+function pickFirst<A>(elements: A[]): A {
+  assert(elements.length === 1, 'List has more than one element')
+
+  return elements[0]
+}
+
+interface TreeNode<ValueType> {
+  value: ValueType
+  children: TreeNode<ValueType>[]
+}
+
+interface EntityWithRevisions extends EntityBase {
+  revisions: Revision[]
+}
+
+interface EntityWithRevision extends EntityBase {
+  revision: Revision
+}
+
+interface Revision {
+  id: number
+  date: Date
+  content: string
+}
+
+interface EntityBase {
+  id: number
   licenseId: number
-  currentRevisionId?: number
+  typeName: TypeName
+}
+
+enum TypeName {
+  ExerciseType = 'text-exercise',
+  SolutionType = 'text-solution',
+}
+
+function isTypeName(values: string): values is TypeName {
+  return Object.values<string>(TypeName).includes(values)
+}
+
+function isNotNull<A>(value: A | null): value is A {
+  return value != null
+}
+
+function printTree<V>(mapper: (x: V) => unknown, node: TreeNode<V>, level = 0) {
+  let result = ''
+
+  for (let i = 0; i < level; i++) result += ' '
+
+  result += String(mapper(node.value))
+
+  console.log(result)
+
+  for (const child of node.children) {
+    printTree(mapper, child, level + 1)
+  }
 }
