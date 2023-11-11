46bdf2ff21fad5ce8476b851191130972084ae1a
Stephan Kulla <git.mail@kulla.me>
2023-11-11 12:44:39 +0100

    feat: Remove revisions as well

--- src/20231111120000-delete-unsupported-entities.ts
+++ src/20231111120000-delete-unsupported-entities.ts
@@ -1,4 +1,4 @@
-import { createMigration } from './utils'
+import { Database, createMigration } from './utils'
 
 const unsupportedEntityTypes = [
   'input-expression-equal-match-challenge',
@@ -19,14 +19,28 @@ createMigration(exports, {
       join type on entity.type_id = type.id
       where type.name in ${toSqlTuple(unsupportedEntityTypes)}
     `)
+    const revisionsToDelete: { id: number }[] = await db.runSql(`
+      select entity_revision.id as id
+      from entity_revision
+      join entity on entity_revision.repository_id = entity.id
+      join type on entity.type_id = type.id
+      where type.name in ${toSqlTuple(unsupportedEntityTypes)}
+    `)
 
-    if (entitiesToDelete.length > 0) {
-      const ids = entitiesToDelete.map((entity) => entity.id)
-      await db.runSql(`DELETE FROM uuid WHERE id IN ${toSqlTuple(ids)}`)
-    }
+    await deleteUuids(db, entitiesToDelete)
+
+    // Let's avoid having revisions not pointing to an apropriate entity
+    await deleteUuids(db, revisionsToDelete)
   },
 })
 
 function toSqlTuple(elements: Array<string | number>): string {
   return '(' + elements.map((e) => JSON.stringify(e)).join(', ') + ')'
+}
+
+async function deleteUuids(db: Database, uuids: { id: number }[]) {
+  if (uuids.length > 0) {
+    const ids = uuids.map((uuid) => uuid.id)
+    await db.runSql(`DELETE FROM uuid WHERE id IN ${toSqlTuple(ids)}`)
+  }
 }
