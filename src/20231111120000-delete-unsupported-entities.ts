58f4eeae0fb8ceeb9d9056500257ef9699fc17d4
Stephan Kulla <git.mail@kulla.me>
2023-11-11 12:40:52 +0100

    refactor: Add `unsupportedEntityTypes`

--- src/20231111120000-delete-unsupported-entities.ts
+++ src/20231111120000-delete-unsupported-entities.ts
@@ -1,21 +1,32 @@
 import { createMigration } from './utils'
 
+const unsupportedEntityTypes = [
+  'input-expression-equal-match-challenge',
+  'input-number-exact-match-challenge',
+  'input-string-normalized-match-challenge',
+  'math-puzzle',
+  'multiple-choice-right-answer',
+  'multiple-choice-wrong-answer',
+  'single-choice-right-answer',
+  'single-choice-wrong-answer',
+]
+
 createMigration(exports, {
   up: async (db) => {
     const entitiesToDelete: { id: number }[] = await db.runSql(`
       select entity.id as id
       from entity
       join type on entity.type_id = type.id
-      where type.name in ("input-expression-equal-match-challenge",
-        "input-number-exact-match-challenge", "input-string-normalized-match-challenge",
-        "math-puzzle", "multiple-choice-right-answer", "multiple-choice-wrong-answer",
-        "single-choice-right-answer", "single-choice-wrong-answer")
+      where type.name in ${toSqlTuple(unsupportedEntityTypes)}
     `)
 
     if (entitiesToDelete.length > 0) {
-      const entitiesList = entitiesToDelete.map((item) => item.id).join(', ')
-
-      await db.runSql(`DELETE FROM uuid WHERE id IN (${entitiesList})`)
+      const ids = entitiesToDelete.map((entity) => entity.id)
+      await db.runSql(`DELETE FROM uuid WHERE id IN ${toSqlTuple(ids)}`)
     }
   },
 })
+
+function toSqlTuple(elements: Array<string | number>): string {
+  return '(' + elements.map((e) => JSON.stringify(e)).join(', ') + ')'
+}
