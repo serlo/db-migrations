03b6b1048d98d8ac5b4aaac69347d979744a8532
Vitomir Budimir <budimirvitomir@gmail.com>
2023-11-14 15:53:58 +0100

    feat: delete events related to unsupported entities

--- src/20231111120000-delete-unsupported-entities.ts
+++ src/20231111120000-delete-unsupported-entities.ts
@@ -28,6 +28,22 @@ createMigration(exports, {
       join type on entity.type_id = type.id
       where type.name in ${toSqlTuple(unsupportedEntityTypes)}
     `)
+    const eventLogsToDelete: { id: number }[] = await db.runSql(`
+      select event_log.id as id
+      from event_log
+      where event_log.uuid_id in ${getUuidsToDelete(
+        entitiesToDelete,
+        revisionsToDelete,
+      )}
+    `)
+    const eventUuidParametersToDelete: { id: number }[] = await db.runSql(`
+      select event_parameter_uuid.id as id
+      from event_parameter_uuid
+      where event_parameter_uuid.uuid_id in ${getUuidsToDelete(
+        entitiesToDelete,
+        revisionsToDelete,
+      )}
+    `)
 
     await deleteUuids(db, apiCache, entitiesToDelete)
     console.log(`INFO: ${entitiesToDelete.length} entities deleted`)
@@ -35,6 +51,14 @@ createMigration(exports, {
     // Let's avoid having revisions not pointing to an apropriate entity
     await deleteUuids(db, apiCache, revisionsToDelete)
     console.log(`INFO: ${revisionsToDelete.length} revisions deleted`)
+
+    await deleteEventLogs(db, eventLogsToDelete)
+    console.log(`INFO: ${eventLogsToDelete.length} event logs deleted`)
+
+    await deleteEventParameterUuids(db, eventUuidParametersToDelete)
+    console.log(
+      `INFO: ${eventUuidParametersToDelete.length} event parameter uuids deleted`,
+    )
 
     await db.runSql(
       `delete from type where name in ${toSqlTuple(unsupportedEntityTypes)}`,
@@ -61,4 +85,40 @@ async function deleteUuids(
       await apiCache.deleteUuid(id)
     }
   }
+}
+
+async function deleteEventLogs(
+  db: Database,
+  // apiCache: ApiCache,
+  event_logs: { id: number }[],
+) {
+  if (event_logs.length > 0) {
+    const ids = event_logs.map((event_log) => event_log.id)
+    await db.runSql(`DELETE FROM event_log WHERE id IN ${toSqlTuple(ids)}`)
+
+    // TODO: Not sure if event logs are cached
+  }
+}
+
+async function deleteEventParameterUuids(
+  db: Database,
+  // apiCache: ApiCache,
+  event_parameter_uuids: { id: number }[],
+) {
+  if (event_parameter_uuids.length > 0) {
+    const ids = event_parameter_uuids.map((uuid) => uuid.id)
+    await db.runSql(
+      `DELETE FROM event_parameter_uuid WHERE id IN ${toSqlTuple(ids)}`,
+    )
+
+    // TODO: Not sure if event parameter uuids are cached
+  }
+}
+
+function getUuidsToDelete(
+  entities: { id: number }[],
+  revisions: { id: number }[],
+) {
+  const ids = [...entities, ...revisions].map((uuid) => uuid.id)
+  return toSqlTuple(ids)
 }
