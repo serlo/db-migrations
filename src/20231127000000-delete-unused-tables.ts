ea8c7212f37fcb123409fad319e2ada47089545c
Andreas Huber <AndreasHuber@users.noreply.github.com>
2023-11-27 01:45:40 +0100

    fix(unused tables): replace syntax that only works with MariaDB but not MySQL

--- src/20231127000000-delete-unused-tables.ts
+++ src/20231127000000-delete-unused-tables.ts
@@ -2,33 +2,48 @@ import { createMigration } from './utils'
 
 createMigration(module.exports, {
   up: async (db) => {
+    // remove reference to the language table we want to delete
+    const result: { count: number }[] = await db.runSql(`
+      SELECT COUNT(*) as count
+      FROM information_schema.columns
+      WHERE table_name = 'instance' AND column_name = 'language_id';
+    `)
+    if (result[0]['count'] > 0) {
+      await db.runSql(`
+          ALTER TABLE instance
+          DROP FOREIGN KEY fk_instance_language1;
+        `)
+      await db.runSql(`
+          ALTER TABLE instance
+          DROP COLUMN language_id;
+        `)
+    }
+
+    // now delete the tables
     await db.runSql(`
-        ALTER TABLE instance
-        DROP COLUMN IF EXISTS language_id;
-      `)
-    await db.runSql(`
-        DROP TABLE IF EXISTS
-          ad,
-          ad_page,
-          instance_permission,
-          language, metadata,
-          metadata_key,
-          navigation_container,
-          navigation_page,
-          navigation_parameter,
-          navigation_parameter_key,
-          page_repository_role,
-          permission,
-          related_content,
-          related_content_category,
-          related_content_container,
-          related_content_external,
-          related_content_internal,
-          role_inheritance,
-          role_permission,
-          session,
-          term_taxonomy_comment, 
-          user_field;
-      `)
+      DROP TABLE IF EXISTS
+        ad,
+        ad_page,
+        instance_permission,
+        language,
+        metadata,
+        metadata_key,
+        navigation_container,
+        navigation_page,
+        navigation_parameter,
+        navigation_parameter_key,
+        page_repository_role,
+        permission,
+        related_content,
+        related_content_category,
+        related_content_container,
+        related_content_external,
+        related_content_internal,
+        role_inheritance,
+        role_permission,
+        session,
+        term_taxonomy_comment, 
+        user_field;
+    `)
   },
 })
