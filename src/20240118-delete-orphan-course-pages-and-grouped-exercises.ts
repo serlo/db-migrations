e30d69ecf5428b40fce13f6f71e85ae0515e7119
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-01-18 13:55:33 +0100

    feat(orphan entities): add migration script to delete orphan entities

---
+++ src/20240118-delete-orphan-course-pages-and-grouped-exercises.ts
@@ -0,0 +1,21 @@
+import { createMigration } from './utils'
+
+createMigration(exports, {
+  up: async (db) => {
+    const uuidsToDelete: { uuid_id: number }[] = await db.runSql(`
+      SELECT id
+      FROM entity
+      WHERE id NOT IN (SELECT child_id FROM entity_link)
+      AND id NOT IN (SELECT entity_id FROM term_taxonomy_entity) 
+      `)
+    if (uuidsToDelete.length > 0) {
+      const uuidsToDeleteSeparatedByComma = uuidsToDelete
+        .map((item) => item.uuid_id)
+        .join(', ')
+      await db.runSql(`
+        DELETE FROM uuid 
+        WHERE id IN (${uuidsToDeleteSeparatedByComma})
+        `)
+    }
+  },
+})
