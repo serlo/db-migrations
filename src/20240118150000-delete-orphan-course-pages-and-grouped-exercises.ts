8487142e650f922151a32cccf495846eae59ed8d
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-01-18 14:46:45 +0100

    refactor(orphan entities): do it all in one query instead of two

--- src/20240118150000-delete-orphan-course-pages-and-grouped-exercises.ts
+++ src/20240118150000-delete-orphan-course-pages-and-grouped-exercises.ts
@@ -2,23 +2,17 @@ import { createMigration } from './utils'
 
 createMigration(exports, {
   up: async (db) => {
-    const uuidsToDelete: { id: number }[] = await db.runSql(`
-      SELECT id
-      FROM entity
-      WHERE type_id in (SELECT id FROM type 
-                        WHERE name="course-page"
-                        OR name="grouped-text-exercise")
-      AND id NOT IN (SELECT child_id FROM entity_link)
-      AND id NOT IN (SELECT entity_id FROM term_taxonomy_entity) 
-      `)
-    if (uuidsToDelete.length > 0) {
-      const uuidsToDeleteSeparatedByComma = uuidsToDelete
-        .map((item) => item.id)
-        .join(', ')
-      await db.runSql(`
-        DELETE FROM uuid 
-        WHERE id IN (${uuidsToDeleteSeparatedByComma})
-        `)
-    }
+    await db.runSql(`
+      DELETE FROM uuid 
+      WHERE id IN (
+        SELECT id
+        FROM entity
+        WHERE type_id IN (SELECT id FROM type 
+                          WHERE name = "course-page"
+                          OR name = "grouped-text-exercise")
+        AND id NOT IN (SELECT child_id FROM entity_link)
+        AND id NOT IN (SELECT entity_id FROM term_taxonomy_entity)
+      )
+    `);
   },
 })
