368a95591de65843da41c385b6cb4684d5c5ef31
Stephan Kulla <git.mail@kulla.me>
2024-02-28 12:52:10 +0100

    feat: Delete keys older than 1,5 months

--- src/20240227000000-delete-cache-keys-older-than-2-months.ts
+++ src/20240227000000-delete-cache-keys-older-than-2-months.ts
@@ -4,7 +4,7 @@ import { ApiCache } from './utils/api-cache'
 createMigration(exports, {
   up: async () => {
     const apiCache = new ApiCache()
-    await apiCache.deleteKeysOlderThan(60 * 60 * 24 * 30 * 2)
+    await apiCache.deleteKeysOlderThan(Math.round(60 * 60 * 24 * 30 * 1.5))
     await apiCache.quit()
   },
 })
