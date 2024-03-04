8b39411b2869779cb351056097d7d398e0dc04a8
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-03-04 13:27:48 +0100

    feat(mathe-pruefungen): delete cache entry

--- src/20240304000000-delete-redirection-mathe-pruefungen.ts
+++ src/20240304000000-delete-redirection-mathe-pruefungen.ts
@@ -1,9 +1,18 @@
 import { createMigration } from './utils'
+import Redis from 'ioredis'
 
 createMigration(exports, {
   up: async (db) => {
     await db.runSql(`
       DELETE FROM url_alias WHERE alias = 'mathe-pruefungen'
       `)
+
+    if (typeof process.env.REDIS_URL === 'string') {
+      const redis = new Redis(process.env.REDIS_URL)
+      redis.del('de.serlo.org/api/alias/mathe-pruefungen')
+      redis.quit()
+    } else {
+      throw new Error('Env `REDIS_URL` is not defined')
+    }
   },
 })
