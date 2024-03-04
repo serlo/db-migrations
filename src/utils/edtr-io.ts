fe03b70710a4965399a3047e1660c2079716e915
Vitomir Budimir <budimirvitomir@gmail.com>
2024-03-04 12:33:19 +0100

    chore(yarn): archive leftover transpiled migration files and delete related deprecated utils

--- src/utils/edtr-io.ts
+++ src/utils/edtr-io.ts
@@ -1,72 +1,4 @@
-import * as t from 'io-ts'
 import * as R from 'ramda'
-
-function updatePlugins(
-  updatePlugin: (
-    plugin: Plugin,
-    applyChangeToChildren: Transformation,
-  ) => Plugin | undefined,
-): Transformation {
-  function applyChangeToChildren(value: unknown): unknown {
-    if (isPlugin(value)) {
-      const newPlugin = updatePlugin(value, applyChangeToChildren)
-
-      if (newPlugin) return newPlugin
-    }
-
-    if (Array.isArray(value)) {
-      return value.map(applyChangeToChildren)
-    }
-
-    if (typeof value === 'object' && value !== null) {
-      return R.mapObjIndexed(applyChangeToChildren, value)
-    }
-
-    return value
-  }
-
-  return applyChangeToChildren
-}
-
-export function transformSlateTypes(transformations: {
-  [key in string]?: ListTransformation<SlateNode>
-}) {
-  return transformLists((value) => {
-    if (SlateNodeDecoder.is(value)) {
-      const transformFunc = transformations[value.type]
-
-      if (typeof transformFunc === 'function') {
-        return transformFunc(value)
-      }
-    }
-  })
-}
-
-function transformLists(
-  transform: ListTransformation<unknown>,
-): Transformation {
-  function applyTransformation(value: unknown): unknown {
-    if (Array.isArray(value)) {
-      const newValue = value.flatMap((element) => {
-        const transformation = transform(element)
-
-        return transformation !== undefined ? transformation : [element]
-      })
-
-      return newValue.map(applyTransformation)
-    }
-
-    if (typeof value === 'object' && value !== null) {
-      return R.mapObjIndexed(applyTransformation, value)
-    }
-
-    return value
-  }
-
-  return applyTransformation
-}
-
-type ListTransformation<A> = (element: A) => A[] | undefined
 
 export function isPlugin(value: unknown): value is Plugin {
   return (
@@ -82,6 +14,3 @@ export interface Plugin {
   plugin: string
   state: unknown
 }
-
-const SlateNodeDecoder = t.type({ type: t.string })
-type SlateNode = t.TypeOf<typeof SlateNodeDecoder>
