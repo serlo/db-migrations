96aee4391f3dba83a2c6c08da1f3a39b9ac99c2d
Stephan Kulla <git.mail@kulla.me>
2024-03-13 01:13:07 +0100

    feat: Use gzip for the log file
    Closes https://github.com/serlo/db-migrations/issues/288

--- src/utils/slack-logger.ts
+++ src/utils/slack-logger.ts
@@ -1,4 +1,5 @@
-import { createWriteStream } from 'fs'
+import { createWriteStream, WriteStream } from 'fs'
+import { createGzip, Gzip } from 'zlib'
 import { tmpdir } from 'os'
 import path from 'path'
 import { WebClient } from '@slack/web-api'
@@ -7,14 +8,19 @@ import { once } from 'events'
 export class SlackLogger {
   private logFileName: string
   private logFilePath: string
-  private logFileStream: ReturnType<typeof createWriteStream>
+  private gz: Gzip
+  private writeStream: WriteStream
 
   constructor(private name: string) {
     const currentDate = new Date().toISOString().slice(0, 10)
 
-    this.logFileName = `${currentDate}-${this.name}.log.jsonl`
+    this.logFileName = `${currentDate}-${this.name}.log.jsonl.gz`
     this.logFilePath = path.join(tmpdir(), this.logFileName)
-    this.logFileStream = createWriteStream(this.logFilePath)
+
+    this.gz = createGzip()
+    this.writeStream = createWriteStream(this.logFilePath)
+
+    this.gz.pipe(this.writeStream)
 
     this.logEvent('logStarted', { name: this.name })
   }
@@ -22,8 +28,8 @@ export class SlackLogger {
   logEvent(eventType: string, data: unknown) {
     const event = { eventType, data, time: new Date().toISOString() }
 
-    this.logFileStream.write(JSON.stringify(event))
-    this.logFileStream.write('\n')
+    this.write(JSON.stringify(event))
+    this.write('\n')
   }
 
   async closeAndSend() {
@@ -34,8 +40,9 @@ export class SlackLogger {
   protected async close() {
     this.logEvent('logEnded', { name: this.name })
 
-    this.logFileStream.end()
-    await once(this.logFileStream, 'finish')
+    this.gz.end()
+    await once(this.gz, 'finish')
+    await once(this.writeStream, 'finish')
   }
 
   protected async send() {
@@ -53,5 +60,11 @@ export class SlackLogger {
       channel_id: channel,
       initial_comment: `*${environment}:* Migration "${this.name}" finished.`,
     })
+  }
+
+  private async write(data: string) {
+    if (!this.gz.write(data)) {
+      await once(this.gz, 'drain')
+    }
   }
 }
