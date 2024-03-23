d010f8a0a86125b599338b749bd59833fad10f95
Stephan Kulla <git.mail@kulla.me>
2024-03-23 11:01:19 +0100

    feat: Add template for new migration with `yarn new`

--- scripts/touch_file.sh
+++ scripts/touch_file.sh
@@ -3,5 +3,16 @@ echo "Enter the name of the migration (no spaces and with hifens)"
 read FILENAME
 
 DATE=$(date +%Y%m%d%H%M00)
+FILENAME="src/${DATE}-${FILENAME}.ts"
 
-touch "src/${DATE}-${FILENAME}.ts"
+echo "import { ApiCache, Database, SlackLogger } from './utils'
+
+export async function up(db: Database) {
+  const apiCache = new ApiCache()
+  const logger = new SlackLogger('${FILENAME}')
+
+  await logger.closeAndSend()
+  // To reduce the time between deleting the keys and finishing the DB
+  // transaction, this should be the last command
+  await apiCache.deleteKeysAndQuit()
+}"> "$FILENAME"
