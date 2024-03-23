daa6d4f936f1d2d2f010c39a12b13a5367688e1d
Stephan Kulla <git.mail@kulla.me>
2024-03-23 10:44:23 +0100

    chore(touch-file): Use proper variable names

--- scripts/touch_file.sh
+++ scripts/touch_file.sh
@@ -1,7 +1,7 @@
 echo "Enter the name of the migration (no spaces and with hifens)"
 
-read filename
+read FILENAME
 
-d=$(date +%Y%m%d%H%M00)
+DATE=$(date +%Y%m%d%H%M00)
 
-touch "src/${d}-${filename}.ts"
+touch "src/${DATE}-${FILENAME}.ts"
