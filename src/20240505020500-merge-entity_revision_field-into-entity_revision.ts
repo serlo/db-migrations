db60142884038d313a9143ba7ea8f8febb802791
Hugo Tiburtino <hugotiburtino@protonmail.com>
2024-06-06 21:42:14 +0200

    fix(entity-field): avoid memory leak

--- src/20240505020500-merge-entity_revision_field-into-entity_revision.ts
+++ src/20240505020500-merge-entity_revision_field-into-entity_revision.ts
@@ -87,19 +87,9 @@ async function logTable({
 
   logger.logEvent('logTable', { tableName, definition })
 
-  while (true) {
-    const rows = await db.runSql<{ id: number }[]>(
-      `select * from ${tableName} where id > ? order by id limit 1000`,
-      lastId,
-    )
-
-    const lastRow = rows.at(-1)
-
-    logger.logEvent('logTable', { tableName, rows })
-
-    if (lastRow == null) break
-    lastId = lastRow.id
-
-    console.log({ lastId })
-  }
+  const rows = await db.runSql<{ id: number }[]>(
+    `select * from ${tableName} where id > ? order by id limit 1000`,
+    lastId,
+  )
+  logger.logEvent('logTable', { tableName, rows })
 }
